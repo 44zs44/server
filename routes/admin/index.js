d35354b5d4c13421f7d772d64e99c2e0d046e5bd
张双 <2265100194@qq.com>
2023-01-03 17:39:01 +0800

    进出接口

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -16,7 +16,7 @@ module.exports = app => {
 //接口
     /*新增分类接口*/
     router.post('/addCategories', async (req, res) => {
-        console.log("新增分类post接口接收数据====>" + JSON.stringify(req.body))
+        console.log("进入新增分类接口,接收数据====>" + JSON.stringify(req.body))
         if (await Category.findOne(req.body)) {
             console.log("查询到重复数据，返回重复数据")
             res.send({message:'添加的数据为已经存在的数据'})
@@ -25,11 +25,13 @@ module.exports = app => {
             const model = await Category.create(req.body)
             res.send(model)
         }
+        console.log("<===退出新增分类接口接收数据")
     })
 
 
-    /*新增获取分类接口*/
+    /*获取分类接口*/
     router.get('/getCategories', async (req, res) => {
+        console.log("进入获取分类接口===>")
         let params = urlLib.parse(req.url, true);
         //有传参
         if (params.query.id) {
@@ -43,12 +45,12 @@ module.exports = app => {
             const model = await Category.find()
             res.send(model)
         }
-
+        console.log("<===退出获取分类接口")
     })
 
     /*删除一条分类接口*/
     router.post('/deleteCategories', async (req, res) => {
-        console.log("进入删除接口")
+        console.log("进入删除分类接口===>")
         console.log(JSON.stringify(req.body))
         if (req.body!=={}){
             const model = await Category.findByIdAndDelete(req.body.id)
@@ -56,23 +58,25 @@ module.exports = app => {
         }else {
             res.send({message:'无传递数据'})
         }
-
+        console.log("<====退出删除分类接口")
     })
 
 
     /*上传图片接口*/
     router.post('/upload',upload.single('file'), async (req, res) => {
+        console.log("进入上传图片接口===>")
         const file=req.file
         file.url=`http://localhost:3000/uploads/${file.filename}`
         console.log(file.url)
         res.send(file)
+        console.log("<===退出上传图片接口")
     })
 
 
 //物品接口
     /*新增物品接口*/
     router.post('/addGoods', async (req, res) => {
-        console.log("新增物品post接口接收数据====>" + JSON.stringify(req.body))
+        console.log("进入新增物品接口,接收数据====>" + JSON.stringify(req.body))
         if (req.body._id) {
             console.log("修改数据==>")
             const model = await Good.findByIdAndUpdate(req.body._id,req.body)
@@ -82,9 +86,12 @@ module.exports = app => {
             const model = await Good.create(req.body)
             res.send(model)
         }
+        console.log("<===退出新增物品接口")
     })
-    /*新获取物品接口*/
+
+    /*获取物品接口*/
     router.get('/getGoodsList', async (req, res) => {
+        console.log("进入获取物品接口====>")
         let params = urlLib.parse(req.url, true);
         //有传参
         if (params.query.id) {
@@ -98,6 +105,7 @@ module.exports = app => {
             const model = await Good.find()
             res.send(model)
         }
+        console.log("<===退出获取物品接口")
     })
 
     /*删除一条物品接口*/
@@ -110,7 +118,7 @@ module.exports = app => {
         }else {
             res.send({message:'无传递数据'})
         }
-
+        console.log("<===退出删除物品接口")
     })
 
 
